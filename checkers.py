bdfff3127dfe04862c25bf42b09e4bb338136e53
t_lycett <lycettthomas94@gmail.com>
2019-02-20 01:32:21 +0000

    change function names to comply with PEP8

--- checkers.py
+++ checkers.py
@@ -1,10 +1,25 @@
 import checkersBoard as board
+import random
 
 b = board.CheckersBoard(True);
-print(b.p1_positions)
-print(b.p2_positions)
-print('valid moves')
-for move in b.getValidMoves(1):
-    print(move.p1_positions)
+for i in range(10):
+    print('p1')
+    print(b.p1_positions)
+    print('p2')
+    print(b.p2_positions)
+    p1_moves = b.get_valid_moves(1)
+    print('p1 valid moves')
+    #for m in p1_moves:
+        #print(m.p1_positions)
+    b.set_positions(random.choice(p1_moves))
+    print('p1 move')
+    print(b.p1_positions)
+    p2_moves = b.get_valid_moves(-1)
+    print('p2 valid moves')
+    #for m in p2_moves:
+        #print(m.p2_positions)
+    b.set_positions(random.choice(p2_moves))
+    print('p2 move')
+    print(b.p2_positions)
 #print(b.p1_kings)
 #print(b.p2_kings)
--- checkersBoard.py
+++ checkersBoard.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+
 class CheckersBoard():
     def __init__(self, start_positions = False, board = None):
         self.reset()
         if start_positions:
-            self.setStartPositions()
+            self.set_start_positions()
         if board is not None:
-            self.setPositions(board)
+            self.set_positions(board)
 
     def reset(self):
         self.p1_positions = np.zeros((8, 8))
@@ -14,7 +15,7 @@ class CheckersBoard():
         self.p1_kings = np.zeros((8, 8))
         self.p2_kings = np.zeros((8, 8))
 
-    def setStartPositions(self):
+    def set_start_positions(self):
         self.reset()
         self.p1_positions[7, ::2] = 1
         self.p1_positions[6, :] = 1
@@ -23,43 +24,43 @@ class CheckersBoard():
         self.p2_positions[0, ::2] = 0
         self.p2_positions[1, ::2] = 1
 
-    def setPositions(self, board):
+    def set_positions(self, board):
         self.p1_positions = board.p1_positions.copy()
         self.p2_positions = board.p2_positions.copy()
         self.p1_kings = board.p1_kings.copy()
         self.p2_kings = board.p2_kings.copy()
 
-    def isValidPosition(self, row, column):
+    def is_valid_position(self, row, column):
         if row >= 0 and row <= 7 and column >= 0 and column <= 7:
             return True
         return False
 
-    def isEmptyPosition(self, row, column):
-        if self.isValidPosition(row, column):
+    def is_empty_position(self, row, column):
+        if self.is_valid_position(row, column):
             if self.p1_positions[row, column] == 0 and self.p2_positions[row, column] == 0:
                 return True
         return False
 
-    def getPlayerPositions(self, player):
+    def get_player_positions(self, player):
         if player == 1:
             return self.p1_positions
         if player == -1:
             return self.p2_positions
         return None
 
-    def getPlayersKings(self, player):
+    def get_players_kings(self, player):
         if player == 1:
             return self.p1_kings
         if player == -1:
             return self.p2_kings
         return None
 
-    def isValidMove(self, player, start, end):
-        if not self.isEmptyPosition(end[0], end[1]) or not self.isValidPosition(start[0], start[1]):
+    def is_valid_move(self, player, start, end):
+        if not self.is_empty_position(end[0], end[1]) or not self.is_valid_position(start[0], start[1]):
             return False
-        player_positions = self.getPlayerPositions(player)
-        player_kings = self.getPlayersKings(player)
-        opponent_positions = self.getPlayerPositions(-(player))
+        player_positions = self.get_player_positions(player)
+        player_kings = self.get_players_kings(player)
+        opponent_positions = self.get_player_positions(-(player))
         if player_positions[start[0], start[1]] == 0: #if no token at start
             return False
         if player_kings[start[0], start[1]] == 0:
@@ -72,58 +73,58 @@ class CheckersBoard():
                 return False
         return True
 
-    def executeMove(self, player, start, end):
-        if not self.isValidMove(player, start, end):
+    def execute_move(self, player, start, end):
+        if not self.is_valid_move(player, start, end):
             return False
-        players_positions = self.getPlayerPositions(player)
-        players_kings = self.getPlayersKings(player)
+        players_positions = self.get_player_positions(player)
+        players_kings = self.get_players_kings(player)
         players_positions[start[0], start[1]] = 0
         players_positions[end[0], end[1]] = 1
         players_kings[end[0], end[1]] = players_kings[start[0], start[1]]
         players_kings[start[0], start[1]] = 0
         return True
 
-    def getValidMovesFromPos(self, player, pos):
+    def get_valid_moves_from_pos(self, player, pos):
         valid_moves = []
         row_offset = -(player)
-        player_positions = self.getPlayerPositions(player)
-        players_kings = self.getPlayersKings(player)
-        opponent_positions = self.getPlayerPositions(-(player))
+        player_positions = self.get_player_positions(player)
+        players_kings = self.get_players_kings(player)
+        opponent_positions = self.get_player_positions(-(player))
         if player_positions[pos[0], pos[1]] == 1:
             x = pos[0] + row_offset
             y = pos[1] - 1
-            if self.isEmptyPosition(x, y):
+            if self.is_empty_position(x, y):
                 new_move = CheckersBoard(board=self)
-                if new_move.executeMove(player, pos, [x, y]):
+                if new_move.execute_move(player, pos, [x, y]):
                     valid_moves.append(new_move)
             y = pos[1] + 1
-            if self.isEmptyPosition(x, y):
+            if self.is_empty_position(x, y):
                 new_move = CheckersBoard(board=self)
-                if new_move.executeMove(player, pos, [x, y]):
+                if new_move.execute_move(player, pos, [x, y]):
                     valid_moves.append(new_move)
             if players_kings[pos[0], pos[1]] == 1:
                 x = pos[0] - row_offset
                 y = pos[1] - 1
-                if self.isEmptyPosition(x, y):
+                if self.is_empty_position(x, y):
                     new_move = CheckersBoard(board=self)
-                    if new_move.executeMove(player, pos, [x, y]):
+                    if new_move.execute_move(player, pos, [x, y]):
                         valid_moves.append(new_move)
                 y = pos[1] + 1
-                if self.isEmptyPosition(x, y):
+                if self.is_empty_position(x, y):
                     new_move = CheckersBoard(board=self)
-                    if new_move.executeMove(player, pos, [x, y]):
+                    if new_move.execute_move(player, pos, [x, y]):
                         valid_moves.append(new_move)
         return valid_moves
 
-    def getValidMoves(self, player):
+    def get_valid_moves(self, player):
         valid_moves = []
-        player_positions = self.getPlayerPositions(player)
-        players_kings = self.getPlayersKings(player)
-        opponent_positions = self.getPlayerPositions(-(player))
+        player_positions = self.get_player_positions(player)
+        players_kings = self.get_players_kings(player)
+        opponent_positions = self.get_player_positions(-(player))
         for row in range(player_positions.shape[0]):
             for column in range(player_positions.shape[1]):
                 if player_positions[row, column] == 1:
-                    new_moves =self.getValidMovesFromPos(player, [row, column])
+                    new_moves = self.get_valid_moves_from_pos(player, [row, column])
                     if len(new_moves) > 0:
                         valid_moves = np.hstack((valid_moves, new_moves))
         return valid_moves
