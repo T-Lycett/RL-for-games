07466224e0b51d5b260a6fde525794ff72aaab2a
t_lycett <lycettthomas94@gmail.com>
2019-02-22 15:50:45 +0000

    implement profiling code

--- checkers.py
+++ checkers.py
@@ -2,32 +2,49 @@ import checkersBoard as board
 import minimaxAgent
 import random
 import time
+import cProfile, pstats, io
+from pstats import SortKey
+
+profile = True
+if profile:
+    pr =cProfile.Profile()
+    pr.enable()
 
 b = board.CheckersBoard(True);
 minimax_agent = minimaxAgent.MinimaxAgent(1, 5)
 done = False
 while not done:
     start = time.time()
-    #for m in p1_moves:
-        #print(m.p1_positions)
-    #b.set_positions(random.choice(p1_moves))
+    # for m in p1_moves:
+    # print(m.p1_positions)
+    # b.set_positions(random.choice(p1_moves))
     p1_move, val = minimax_agent.get_move(b)
     b.set_positions(p1_move)
     print('p1 move')
     print(b.p1_positions - b.p2_positions)
     print(val)
-    #time.sleep(1)
+    # time.sleep(1)
     done, _ = b.game_ended()
     if not done:
         p2_moves = b.get_valid_moves(-1)
         print('p2 valid moves')
-        #for m in p2_moves:
+        # for m in p2_moves:
         # #print(m.p2_positions)
         b.set_positions(random.choice(p2_moves))
         print('p2 move')
         print(b.p1_positions - b.p2_positions)
         done, _ = b.game_ended()
-    #time.sleep(1)
+    # time.sleep(1)
     print(time.time() - start)
+    done = True
+if profile:
+    # profiling code from: https://docs.python.org/3/library/profile.html#module-profile
+    pr.disable()
+    s = io.StringIO()
+    sortby = SortKey.CUMULATIVE
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
+
 #print(b.p1_kings)
 #print(b.p2_kings)
