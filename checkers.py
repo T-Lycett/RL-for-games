f02167c80b6fce8059c7b112df34b3ca0687d98c
t_lycett <lycettthomas94@gmail.com>
2019-02-22 00:59:38 +0000

    implement minimax agent
    closes #3

--- checkers.py
+++ checkers.py
@@ -1,20 +1,21 @@
 import checkersBoard as board
+import minimaxAgent
 import random
+import time
 
 b = board.CheckersBoard(True);
+minimax_agent = minimaxAgent.MinimaxAgent(1)
 done = False
 while not done:
-    print('p1')
-    print(b.p1_positions)
-    print('p2')
-    print(b.p2_positions)
     p1_moves = b.get_valid_moves(1)
-    print('p1 valid moves')
     #for m in p1_moves:
         #print(m.p1_positions)
-    b.set_positions(random.choice(p1_moves))
+    #b.set_positions(random.choice(p1_moves))
+    p1_move, _ = minimax_agent.minimax(b)
+    b.set_positions(p1_move)
     print('p1 move')
     print(b.p1_positions)
+    time.sleep(1)
     done, _ = b.game_ended()
     if not done:
         p2_moves = b.get_valid_moves(-1)
@@ -25,5 +26,6 @@ while not done:
         print('p2 move')
         print(b.p2_positions)
         done, _ = b.game_ended()
+    time.sleep(1)
 #print(b.p1_kings)
 #print(b.p2_kings)
---
+++ minimaxAgent.py
@@ -0,0 +1,42 @@
+import checkersBoard
+import numpy as np
+
+class MinimaxAgent:
+    def __init__(self, player, max_depth = 5):
+        self.player = player #1 = player1, -1 = player 2
+        self.max_depth = max_depth
+
+    def minimax(self, board, current_depth=0):
+        game_ended, _ = board.game_ended()
+        if current_depth >= self.max_depth or game_ended:
+            return board, self.evaluate(board)
+        if current_depth % 2 == 0:
+            current_player = self.player
+        else:
+            current_player = -self.player
+        actions = board.get_valid_moves(current_player)
+        scores = np.ndarray([len(actions)])
+        for i, a in enumerate(actions):
+            _, scores[i] = self.minimax(a, current_depth + 1)
+        if current_player == self.player:
+            return actions[np.argmax(scores)], np.max(scores)
+        else:
+            return actions[np.argmin(scores)], np.min(scores)
+
+
+    def evaluate(self, board):
+        has_ended, winner = board.game_ended()
+        if has_ended:
+            return 100 * self.player * winner
+
+        score = 0
+        for n in board.get_player_positions(self.player).flat:
+            score += n
+        for n in board.get_player_positions(-self.player).flat:
+            score -= n
+        for k in board.get_players_kings(self.player).flat:
+            score += k * 0.5
+        for k in board.get_players_kings(-self.player).flat:
+            score -= k * 0.5
+
+        return score
