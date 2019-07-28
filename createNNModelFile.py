6d26d27e42a57055cd95296146cdde9a6bd699b5
t_lycett <lycettthomas94@gmail.com>
2019-07-28 18:54:14 +0100

    train res64x3 network

--- createNNModelFile.py
+++ createNNModelFile.py
@@ -3,7 +3,7 @@ import resNN
 
 
 def create_model_file(filename):
-    model = resNN.ResNN(lr=0.0001, residual_blocks=5, width=128, q_learning_only=True)
+    model = resNN.ResNN(lr=0.00001, residual_blocks=3, width=64, q_learning_only=True)
     model.save_model(filename)
 
 
